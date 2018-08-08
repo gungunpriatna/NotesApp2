78061205ddd3e9f8c60457b6fe8556486cd2e82b
Sonya Luvita <gungunpriatna02@gmail.com>
2018-08-08 14:58:33 +0700

    modified database contract sama helper

--- app/src/main/java/me/gungunpriatna/notesapp/db/DatabaseContract.java
+++ app/src/main/java/me/gungunpriatna/notesapp/db/DatabaseContract.java
@@ -1,5 +1,7 @@
 package me.gungunpriatna.notesapp.db;
 
+import android.database.Cursor;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class DatabaseContract {
@@ -10,5 +12,20 @@ public class DatabaseContract {
         static String TITLE = "title";
         static String DESCRIPTION = "description";
         static String DATE = "date";
+    }
+
+    public static final String AUTHORITY = "com.dicoding.mynotesapp";
+    public static final Uri CONTENT_URI = new Uri.Builder().scheme("content")
+            .authority(AUTHORITY)
+            .appendPath(TABLE_NOTE)
+            .build();
+    public static String getColumnString(Cursor cursor, String columnName) {
+        return cursor.getString( cursor.getColumnIndex(columnName) );
+    }
+    public static int getColumnInt(Cursor cursor, String columnName) {
+        return cursor.getInt( cursor.getColumnIndex(columnName) );
+    }
+    public static long getColumnLong(Cursor cursor, String columnName) {
+        return cursor.getLong( cursor.getColumnIndex(columnName) );
     }
 }
