d8aea55ee8c471176fa1ac0a3f88643a68c25ff6
Sonya Luvita <gungunpriatna02@gmail.com>
2018-08-08 16:40:33 +0700

    add modul

---
+++ dicodingnotesapp/src/main/java/me/gungunpriatna/dicodingnotesapp/db/DatabaseContract.java
@@ -0,0 +1,32 @@
+package me.gungunpriatna.dicodingnotesapp.db;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class DatabaseContract {
+
+    public static String TABLE_NOTE = "note";
+
+    public static final class NoteColumns implements BaseColumns {
+        public static String TITLE = "title";
+        public static String DESCRIPTION = "description";
+        public static String DATE = "date";
+    }
+
+    public static final String AUTHORITY = "me.gungunpriatna.notesapp";
+    public static final Uri CONTENT_URI = new Uri.Builder().scheme("content")
+            .authority(AUTHORITY)
+            .appendPath(TABLE_NOTE)
+            .build();
+    public static String getColumnString(Cursor cursor, String columnName) {
+        return cursor.getString( cursor.getColumnIndex(columnName) );
+    }
+    public static int getColumnInt(Cursor cursor, String columnName) {
+        return cursor.getInt( cursor.getColumnIndex(columnName) );
+    }
+    public static long getColumnLong(Cursor cursor, String columnName) {
+        return cursor.getLong( cursor.getColumnIndex(columnName) );
+    }
+}
+
